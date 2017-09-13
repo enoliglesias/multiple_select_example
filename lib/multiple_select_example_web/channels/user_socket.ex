d9acfe0c21a607424ce3800bd22787a4becc8280
Enol Iglesias <enoliglesias@gmail.com>
2017-09-13 22:02:41 +0200

    Fix missing coma

--- lib/multiple_select_example_web/channels/user_socket.ex
+++ lib/multiple_select_example_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule MultipleSelectExampleWeb.UserSocket do
   # channel "room:*", MultipleSelectExampleWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
     timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
